36e42e53706c1c60beda52121da1c31f56ce3342
Molnár Attila <molnar.attila@szlgbp.hu>
2023-02-16 21:05:23 +0100

    innerhtml-es kész

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-let 
+main()
+
+function main(){
+    gomb.addEventListener('click', katt);
+    gomb2.addEventListener('click', katt2);
+}
+
+function katt(){
+    let g = document.querySelector('.grid-container');
+    let r = Math.round(1+Math.random()*10);
+    g.innerHTML += `<div class="grid-item">${r}</div>`;
+}
+
+function katt2(){
+    alert('most DOM-ot bővítek');
+}
